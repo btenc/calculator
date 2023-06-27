a84c19ebcd1c27686ae18ced695878fa2f11379d
William Ten Cate <billytencate@gmail.com>
2023-06-27 15:42:44 -0400

    Create operation functions

---
+++ script.js
@@ -0,0 +1,39 @@
+function operate(a,b,op){
+    if(typeof(a) == 'number' && typeof(b) == 'number' && (op == "+" || op == "-" || op == "*" || op == "/")){
+        if(op == "+"){
+            return add(a,b);
+        }
+        else if(op == "-"){
+            return subtract(a,b);
+        }
+        else if(op == "*"){
+            return multiply(a,b);
+        }
+        else if(op == "/"){
+            return divide(a,b);
+        }
+        else{
+            return "ERROR";
+        }
+    }
+    else{
+        return "ERROR";
+    }
+
+}
+
+function add(a,b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
